a342a4682e0b8222fb30c85a5f6b6bb9f5055273
mahesh-muttinti <maheshmuttinti@gmail.com>
2022-01-13 13:17:16 +0530

    refactor: seekers code & changes: seekers table schema

--- src/repositories/seekers.js
+++ src/repositories/seekers.js
@@ -16,13 +16,7 @@ const create = async (payload) => {
 
 const getAllSeekers = async (query) => {
   try {
-    if (query && query.tags && query.tags.length > 0) {
-      var arr = query.tags.split(",");
-      return await getSeekersByTags(arr);
-    } else {
-      const rows = knex("seekers").orderBy("created_at", "desc");
-      return rows;
-    }
+    return await knex("seekers").orderBy("created_at", "desc");
   } catch (error) {
     throw error;
   }
@@ -32,24 +26,13 @@ const findByUuid = async (where = {}) => {
   return await baseRepo.first("seekers", where);
 };
 
-const getAllPostedSeekers = async (user_uuid) => {
-  try {
-    const rows = knex("seekers").where("user_uuid", user_uuid);
-    return rows;
-  } catch (error) {
-    throw error;
-  }
-};
-
 const remove = async (id) => {
   const query = await knex("seekers").where({ uuid: id });
   if (query.length === 0) {
     return { message: "doesn't exist" };
   } else {
     const where = { uuid: id };
-    //   await knex('seekers_tags').where({ query_uuid: where.uuid }).delete();
     await baseRepo.remove("seekers", where, "hard");
-    //   await baseRepo.remove("seekers", where, "soft");
     return { message: "success" };
   }
 };
@@ -75,7 +58,6 @@ module.exports = {
   create,
   getAllSeekers,
   findByUuid,
-  getAllPostedSeekers,
   remove,
   update,
 };
